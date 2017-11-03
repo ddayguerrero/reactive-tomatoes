3bea7c1573699a2e106b57d3179fbffe57b423ea
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-11-02 23:58:57 -0400

    Configure tools and dev enviroment

---
+++ js/clientapp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    null,
+    ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+    ce(MyTitle, { color: 'peru', title: 'Orange is the New Black' }),
+    ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+  );
+};
+
+render(ce(MyFirstComponent), document.getElementById('app'));
