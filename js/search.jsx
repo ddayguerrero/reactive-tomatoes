a5a93805fc5a63cac2dc9c7eb6669403d45a3914
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-11-28 02:13:30 -0500

    Change to ES6 class components

--- js/search.jsx
+++ js/search.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import ShowCard from './showcard';
-import preload from '../data.json';
+import React, { Component } from "react";
+import ShowCard from "./showcard";
+import preload from "../data.json";
 
-const Search = () => (
-  <div className='search'>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} /> )}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Reactive Tomatoes</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
