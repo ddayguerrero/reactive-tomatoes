4ce1dd0ef31befe29167835cdf493485c5f48bcd
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-12-04 00:57:41 -0500

    Remove property types and use flow typings

--- js/showcard.jsx
+++ js/showcard.jsx
@@ -1,5 +1,6 @@
+// @flow
+
 import React from 'react';
-import { string } from 'prop-types';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -17,7 +18,12 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-const ShowCard = props => (
+const ShowCard = (props: {
+    poster: string,
+    title: string,
+    year: string,
+    description: string
+  }) => (
   <Wrapper>
   <Image alt={`${props.title} Show poster`} src={`/public/img/posters/${props.poster}`} />
     <div>
@@ -27,12 +33,5 @@ const ShowCard = props => (
     </div>
   </Wrapper>
 );
-
-ShowCard.propTypes = {
-  poster: string.isRequired,
-  title: string.isRequired,
-  year: string.isRequired,
-  description: string.isRequired
-}
 
 export default ShowCard;
