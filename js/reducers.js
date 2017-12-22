b6f1338e47fd4c58031a5dcc9dc686506a423b13
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-12-21 23:02:57 -0500

    Create getAPIData reducer

--- js/reducers.js
+++ js/reducers.js
@@ -1,6 +1,6 @@
 // @flow
 import { combineReducers } from "redux";
-import { SET_SEARCH_TERM } from "./actions";
+import { SET_SEARCH_TERM, ADD_API_DATA } from "./actions";
 
 const searchTerm = (state = '', action: Action) => {
   if (action.type === SET_SEARCH_TERM) {
@@ -9,6 +9,13 @@ const searchTerm = (state = '', action: Action) => {
   return state;
 };
 
-const rootReducer = combineReducers({ searchTerm });
+const apiData = (state = {}, action: Action) => {
+  if (action.type === ADD_API_DATA) {
+    return Object.assign({}, state, { [action.payload.imdbID]: action.payload });
+  }
+  return state;
+}
+
+const rootReducer = combineReducers({ searchTerm, apiData });
 
 export default rootReducer;
